70391e6366a27d87ba0d96ecad3e59d4693ae2e2
AllaSmyshlyaeva <alla.smyshlyaeva@gmail.com>
2017-10-30 17:06:21 +0200

    update template

--- polls/views.py
+++ polls/views.py
@@ -2,20 +2,22 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.db import connections
 from django.template.loader import render_to_string
+from polls.models import Libruary
 
 @csrf_exempt
 def index(request):
-
-    cursor = connections['default'].cursor()
-    cursor.execute("SELECT " + \
-            "libruary.track_no," + \
-            "libruary.artist," + \
-            "libruary.track_name," + \
-            "genre.name AS genre_name" + \
-        " FROM libruary" + \
-        " JOIN genre ON (libruary.genre = genre.id)")
     
-    rows = dictfetchall(cursor)
+    #cursor = connections['default'].cursor()
+    #cursor.execute("SELECT " + \
+    #        "libruary.track_no," + \
+    #       "libruary.artist," + \
+    #       "libruary.track_name," + \
+    #       "genre.name AS genre_name" + \
+    #   " FROM libruary" + \
+    #   " LEFT JOIN genre ON (libruary.genre = genre.id)")
+    
+    rows = Libruary.objects.all()
+    # dictfetchall(cursor)
     
     result = render_to_string('index.html', {
         'my_string': 'Dima',
@@ -24,13 +26,43 @@ def index(request):
     
     return HttpResponse(result)
 
+
+
+
+
 @csrf_exempt
 def add(request):
+    track = {}
+    if request.method == 'POST':
+        new_track = Libruary(
+            track_no = 1, 
+            track_name = request.POST.get("track_name",""),
+            artist = request.POST.get("artist",""), 
+            album = 1,
+            genre = 1)
+        
+        new_track.save()
+        
+        #track['track_name'] = request.POST.get("track_name","")
+        #track['artist'] = request.POST.get("artist","")
+        
+        #cursor = connections['default'].cursor()
+        
+        #     "INSERT INTO libruary SET track_name = 'Baby', 
+        #     artist = 'Biber'"
     
+        #cursor.execute("INSERT INTO libruary SET track_name = '"  + \
+        #track['track_name'] + "', artist = '" + track['track_name'] + "'")
 
-    result = render_to_string('add.html', {})
+    result = render_to_string('add.html', track)
     
     return HttpResponse(result)
+
+
+
+
+
+
 
 def dictfetchall(cursor):
     "Return all rows from a cursor as a dict"
